b9a0df55fc0ce91c22053f9a83fcb64564acfaad
mominul <ulhouqemomin39@gmail.com>
2023-11-24 12:36:31 +0600

    added user services model

---
+++ src/app/modules/students/user.service.ts
@@ -0,0 +1,24 @@
+import { UserDetails } from './user.interface';
+import UserDetailsModel from './user.model';
+
+const createUserDetailsIntoDB = async (user: UserDetails) => {
+  const result = await UserDetailsModel.create(user);
+  return result;
+};
+
+const getAllUserFromDB = async () => {
+  const result = await UserDetailsModel.find();
+  return result;
+};
+
+const getSingelUserDetailsFromDB = async (userId: string) => {
+  const result = await UserDetailsModel.findOne({ id: userId });
+  return result;
+};
+
+export const usersServices = {
+    createUserDetailsIntoDB,
+    getAllUserFromDB,
+    getSingelUserDetailsFromDB,
+    
+}
