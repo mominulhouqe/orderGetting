4dca16268ae0f5ab5aad05f5149941a977d3f37c
mominul <ulhouqemomin39@gmail.com>
2023-11-24 11:53:03 +0600

    added setup

---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// eslint-disable-next-line no-undef
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  // eslint-disable-next-line no-undef
+  port: process.env.PORT,
+  // eslint-disable-next-line no-undef
+  database_url: process.env.DATABASE_URL,
+};
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from './app';
+import config from './app/config';
+
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+}
+
+main();
